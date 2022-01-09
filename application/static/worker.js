77b18c38cb40308fc8a581675f12e55d13f1d0d5
Igor Agapov <iagapov@rambler.ru>
2022-01-09 19:04:13 +0300

    fix: worker.js

--- application/static/worker.js
+++ application/static/worker.js
@@ -1,4 +1,6 @@
-const files = [
+// Change version here in case of static content updates
+const CACHE_NAME = 'metarhia-static-v8';
+const CACHE_FILES = [
   '/',
   '/console.css',
   '/events.js',
@@ -10,16 +12,69 @@ const files = [
   '/metarhia.svg',
 ];
 
+/*
 self.addEventListener('install', async (event) => {
   const cache = await caches.open('metarhia');
-  event.waitUntil(cache.addAll(files));
+  event.waitUntil(await cache.addAll(CACHE_FILES));
 });
 
-self.addEventListener('fetch', async ({ request }) => {
-  const cache = await caches.open('metarhia');
-  const cached = await cache.match(request);
+*/
+
+const registerContent = async () => {
+  const cache = await caches.open(CACHE_NAME);
+  // console.log('registerContent...');
+  return cache.addAll(CACHE_FILES);
+};
+
+const checkVersionUpdates = async () => {
+  const cacheWhitelist = [CACHE_NAME];
+  const cacheNames = await caches.keys();
+  return Promise.all(
+    cacheNames.map((name) => {
+      if (!cacheWhitelist.includes(name)) return caches.delete(name);
+    })
+  );
+};
+
+/*
+self.addEventListener('install', async (event) => {
+  console.log(files);
+  event.waitUntil(
+    caches.open('metarhia').then((cache) => cache.addAll(files))
+  );
+});
+*/
+
+const getContent = async (request) => {
+  const cached = await caches.match(request);
   if (cached) return cached;
   const response = await fetch(request);
-  if (response.status < 400) await cache.put(request, response.clone());
+  if (response && response.status === 200 && response.type === 'basic') {
+    const cache = await caches.open(CACHE_NAME);
+    await cache.put(request, response.clone());
+  }
   return response;
+};
+
+self.addEventListener('install', (event) => {
+  console.log('install');
+  const contentRegistration = registerContent();
+  event.waitUntil(contentRegistration);
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('activate');
+  const versionChecking = checkVersionUpdates();
+  event.waitUntil(versionChecking);
+});
+
+self.addEventListener('fetch', (event) => {
+  // console.log('fetch');
+  // caches.keys().then((e) => console.log(e));
+  const request = event.request;
+  // console.log(request.url);
+  if (request.url.startsWith('http')) {
+    const contentRetrieving = getContent(request);
+    event.respondWith(contentRetrieving);
+  }
 });
